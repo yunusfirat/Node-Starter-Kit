3d045883638234d55663ec556727d820b5f7a7bb
yunusfirat <yunusfirat44@gmail.com>
2021-05-17 22:52:08 +0100

    creating Members and playing around

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id:1,
+        name:"Joe Doe",
+        email:"john@gmai.com",
+        status:"inactive",
+    },
+    {
+        id:2,
+        name:"Cem Doe",
+        email:"cem@gmai.com",
+        status:"active",
+    },
+    {
+        id:3,
+        name:"Yunus Doe",
+        email:"yunus@gmai.com",
+        status:"inactive",
+    },
+    {
+        id:4,
+        name:"ali Doe",
+        email:"ali@gmai.com",
+        status:"active",
+    },
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT =  process.env.PORT || 3000;
 
-
+const members = require("./Members");
 app.get("/",(req,res)=>{
     res.send(`<h1>Hello world!!!!!!</h1>`)
 })
@@ -15,3 +15,19 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 //     "start": "node index",
 //     "dev": "nodemon index"
 //   }
+
+// get all members
+app.get("/api/members", (req, res) => {
+    res.json(members);
+})
+
+// get single member
+
+app.get("/api/members/:id", (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found){
+        res.json(members.filter(member =>  member.id === parseInt(req.params.id)))
+    }else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+    } 
+})
