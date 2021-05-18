9d905683343e819e94ec1d1665207fb767921167
yunusfirat <yunusfirat44@gmail.com>
2021-05-18 02:34:43 +0100

    delete method and handlebars

--- routes/api/members.js
+++ routes/api/members.js
@@ -58,5 +58,18 @@ router.put("/:id", (req,res) => {
     } 
 })
 
+// Delete member
+router.delete("/:id", (req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    if(found){
+        res.json({
+            msg: "member deleted",
+            members: members.filter(member =>  member.id !== parseInt(req.params.id))
+        });
+    }else {
+        res.status(400).json({ msg: `No member with the id of ${req.params.id}`});
+    } 
+});
+
 
 module.exports = router;
